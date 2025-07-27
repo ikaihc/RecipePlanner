c8fc2e7abba0f4fe014ff7d0fd8736f58dafc66a
Rachel Xing <xing0023@algonquinlive.com>
2025-07-26 22:45:55 -0400

    feat: Created Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import './App.css'
 import AllRecipes from './pages/AllRecipes.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import RecipeDetail from './pages/RecipeDetail.jsx'
+import Home from './pages/Home.jsx'
 
 function App () {
     const router = createBrowserRouter([
+        { path: '/', element: <Home/> },
         { path: '/all-recipes', element: <AllRecipes/> },
         { path: '/:recipeId/detail', element: <RecipeDetail/> },
     ])
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/common/Layout.jsx'
+
+function Home (props) {
+    return (
+        <Layout>
+            <h2 className="text-3xl text-indigo-500 font-semibold text-center p-10">Welcome to our recipe planner website</h2>
+        </Layout>
+    )
+}
+
+export default Home
