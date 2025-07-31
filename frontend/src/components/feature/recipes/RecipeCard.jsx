4ff698c2fcd518e267dc0161b8720881fd8db792
Rachel Xing <xing0023@algonquinlive.com>
2025-07-31 17:25:30 -0400

    feat: Implemented add/remove favorites in favourites page

--- frontend/src/components/feature/recipes/RecipeCard.jsx
+++ frontend/src/components/feature/recipes/RecipeCard.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../../common/Button.jsx'
 import { Link } from 'react-router'
 import { useRecipes } from '../../../contexts/RecipeContext.jsx'
+import { IoIosHeart, IoIosHeartEmpty } from 'react-icons/io'
 
-function RecipeCard ({ id,title, description, image_url ,edit}) {
-    const {deleteRecipe,loadRecipes,loadUserRecipes} = useRecipes()
-
-    // auth user after login (plan to get from authUser context)
-    const authUser = {
-        user_id:11
-    }
-
+function RecipeCard ({ id, title, description, image_url, edit, page }) {
+    const { deleteRecipe, loadRecipes, loadUserRecipes, loadFavorites, toggleFavorite } = useRecipes()
+    const [isFavorite, setIsFavorite] = useState()
     // after delete recipes, refetch all recipes and user recipes
-    const handleDeleteRecipe = async()=> {
+    const handleDeleteRecipe = async() => {
         alert('Please confirm to delete the recipe')
         await deleteRecipe(id)
         await loadRecipes()
-        await loadUserRecipes(authUser.user_id)
+        await loadUserRecipes()
     }
+
+    useEffect(() => {
+        ( async function () {
+
+            const favs = await loadFavorites()
+            const isFav = favs?.some(fav => fav.id === id)
+            setIsFavorite(isFav)
+        } )()
+    }, [])
+
+    const handleToggleFavorites = async() => {
+
+        await toggleFavorite(id)
+        const updatedFavorites = await loadFavorites()
+
+        const isFav = updatedFavorites?.some(fav => fav.id === id)
+        setIsFavorite(isFav)
+    }
+
     return (
         <div
             className="flex flex-col justify-between w-full max-w-xs bg-white border border-gray-200 rounded-sm shadow hover:shadow-xl transition-shadow duration-200">
@@ -27,22 +42,30 @@ function RecipeCard ({ id,title, description, image_url ,edit}) {
                 className="w-full h-48 object-cover rounded-t-sm"
             />
             <div className="p-4">
-                <h3 className="text-lg font-semibold text-gray-800 text-center truncate">
-                    { title }
-                </h3>
+                <div className="flex justify-between ">
+                    <h3 className="text-lg font-semibold text-gray-800 text-center truncate">
+                        { title }
+                    </h3>
+
+                    { page === 'favorites' && ( <div className="cursor-pointer" onClick={ handleToggleFavorites }>
+                        { isFavorite ? <IoIosHeart size={ 24 } color="red"/> : <IoIosHeartEmpty size={ 24 }/> }
+                    </div> ) }
+
+
+                </div>
                 <p className="mt-2 text-sm text-gray-600 line-clamp-3">
                     { description }
                 </p>
             </div>
-            <div className={ `p-4 ${edit ? 'flex gap-2' : ''}` }>
-                <Link to={ `/${ id }/detail` } className='flex-1'>
+            <div className={ `p-4 ${ edit ? 'flex gap-2' : '' }` }>
+                <Link to={ `/${ id }/detail` } className="flex-1">
                     <Button>View Detail</Button>
                 </Link>
-                {edit &&  <Link to={ `/${ id }/update-recipe` } className='flex-1'>
+                { edit && <Link to={ `/${ id }/update-recipe` } className="flex-1">
                     <Button>Update</Button>
-                </Link>}
-                <div className='flex-1'>
-                    { edit && <Button onClick={handleDeleteRecipe}>Delete</Button> }
+                </Link> }
+                <div className="flex-1">
+                    { edit && <Button onClick={ handleDeleteRecipe }>Delete</Button> }
                 </div>
 
             </div>
--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -19,7 +19,7 @@ function AllRecipes () {
             <h1 className="text-4xl font-semibold text-gray-800 my-5 text-center">Favourites</h1>
             <div className="flex flex-wrap gap-10 p-10 justify-center">
                 { favorites.length > 0 ? favorites.map((recipe, idx) => (
-                    <RecipeCard key={ idx } { ...recipe }/>
+                    <RecipeCard key={ idx } { ...recipe } page='favorites'/>
                 )) : <h2 className="text-3xl text-indigo-500 font-semibold text-center p-10">No favourites added yet</h2> }
             </div>
 
