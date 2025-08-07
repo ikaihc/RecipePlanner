0b4750dc7bc2b9ccaa8747da48409e54a6d63950
Rachel Xing <xing0023@algonquinlive.com>
2025-08-07 11:39:20 -0400

    feat: modified background color

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -4,12 +4,12 @@ import { Outlet } from 'react-router-dom'
 
 function Layout () {
     return (
-        <>
+        <div className='flex flex-col h-dvh'>
             <NavBar/>
-            <main className='pt-16'>
+            <main className='pt-16 bg-gray-50 flex-1 overflow-auto'>
                 <Outlet/>
             </main>
-        </>
+        </div>
     )
 }
 
